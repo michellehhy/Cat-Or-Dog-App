7bb75a5080d7b95fa1ab31abbe47e06e6b3a560b
Michelle Ho <homichelle724@gmail.com>
2021-10-01 11:01:04 +0100

    Initial Commit

---
+++ Cat or Dog/Cat_or_DogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_or_DogApp.swift
+//  Cat or Dog
+//
+//  Created by Michelle on 01/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Cat_or_DogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
